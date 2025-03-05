a2fe7ac4064eff758ba23a47e9b5beb143d50cdb
Tyshchenko Oleksandra <tyshchenko.oleksandra@lll.kpi.ua>
2025-03-05 22:48:10 +0200

    LR2 UPDATE

--- WinForms_Hotel/WinForms_Hotel/Form1.cs
+++ WinForms_Hotel/WinForms_Hotel/Form1.cs
@@ -8,3 +8,80 @@ namespace WinForms_Hotel
         }
     }
 }
+abstract class Hotel
+{
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public string Description { get; set; }
+
+    public abstract void ShowHotelRooms();
+
+    public virtual void ShowHotelInfo()
+    {
+        Console.WriteLine($"Готель: {Name}, Локація: {Location}, Опис: {Description}");
+    }
+
+
+    public Hotel(string name, string location, string description)
+    {
+        Name = name;
+        Location = location;
+        Description = description;
+    }
+}
+
+interface IUser
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public bool Admin { get; set; }
+}
+
+class Room
+{
+    public string TypeOfRoom { get; }
+    public int MaxGuests { get; }
+    public float Price { get; set; }
+    public bool Availability { get; set; }
+
+    public virtual void ShowRoomInfo()
+    {
+        Console.WriteLine($"Тип номера: {TypeOfRoom}, Кількість гостей: {MaxGuests}, ціна: {Price}, доступність: {Availability}");
+    }
+
+
+    public Room(string typeOfRoom, int maxGuests, float price, bool availability)
+    {
+        TypeOfRoom = typeOfRoom;
+        MaxGuests = maxGuests;
+        Price = price;
+        Availability = availability;
+    }
+}
+
+interface IBooking
+{
+    public void ConfirmBooking();
+    public void CancelBooking();
+}
+
+class Booking : IBooking
+{
+    public Hotel Hotel { get; set; }
+    public IUser User { get; set; }
+    public Room Room { get; set; }
+    public string DateIn { get; set; }
+    public string DateOut { get; set; }
+    public string Status { get; set; }
+
+    public void ConfirmBooking()
+    {
+        Status = "booked";
+        Console.WriteLine("Бронювання виконане");
+    }
+    public void CancelBooking()
+    {
+        Status = "canceled";
+        Console.WriteLine("Бронювання скасоване");
+    }
+}
--- WinForms_Hotel/WinForms_Hotel/obj/Debug/net8.0-windows/WinForms_Hotel.AssemblyInfo.cs
+++ WinForms_Hotel/WinForms_Hotel/obj/Debug/net8.0-windows/WinForms_Hotel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinForms_Hotel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c912e7d38cc030c00b58bffda3d27b5b442dee1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+411a552e3a306a838ccd9d574932128ca0ace002")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinForms_Hotel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinForms_Hotel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
