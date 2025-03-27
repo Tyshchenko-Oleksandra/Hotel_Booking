88c11e719e28da3ccff0c6ea91fd4b39567b65dd
Dmytro <dimasscorpion1011@gmail.com>
2025-03-27 19:44:35 +0200

    Add Folders

---
+++ WinForms_Hotel/WinForms_Hotel/Classes/Room.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinForms_Hotel.Classes
+{
+    class Room : BaseEntity
+    {
+        public string TypeOfRoom { get; }
+        public int MaxGuests { get; }
+        public float Price { get; set; }
+        public bool Availability { get; set; }
+
+        public virtual void ShowRoomInfo()
+        {
+            Console.WriteLine($"Тип номера: {TypeOfRoom}, Кількість гостей: {MaxGuests}, ціна: {Price}, доступність: {Availability}");
+        }
+
+
+        public Room(string typeOfRoom, int maxGuests, float price, bool availability, int id) : base(id)
+        {
+            TypeOfRoom = typeOfRoom;
+            MaxGuests = maxGuests;
+            Price = price;
+            Availability = availability;
+        }
+    }
+}
